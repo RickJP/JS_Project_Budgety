5d8b62a5486cdd67a63ff333f2c1d19bb0500cb7
Rick <3741326+RickJP@users.noreply.github.com>
2019-03-27 21:31:19 +0900

    Now has appropriate color focus for input boxes and button (green for income, and red for expenses)

--- app.js
+++ app.js
@@ -189,6 +189,12 @@ var UIController = (function() {
 
     };
 
+    var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+          callback(list[i], i);
+        }
+    };
+
 
 	return {
 		getInput: function() {
@@ -270,12 +276,6 @@ var UIController = (function() {
 
       fields = document.querySelectorAll(DOMStr.expensesPercentageLabel);
 
-      var nodeListForEach = function(list, callback) {
-        for (var i = 0; i < list.length; i++) {
-          callback(list[i], i);
-        }
-      };
-
       nodeListForEach(fields, function(current, index){
 
         if (percentages[index] > 0) {
@@ -283,8 +283,6 @@ var UIController = (function() {
         } else {
             current.textContent = '---';
         }
-       
-
       });
     },
 
@@ -299,6 +297,18 @@ var UIController = (function() {
       document.querySelector(DOMStr.dateLabel).textContent = months[month] + ' ' + year;
     },
     
+    changedType: function() {
+      var fields = document.querySelectorAll(
+        DOMStr.inputType + ',' + 
+        DOMStr.inputDescription + ',' +
+        DOMStr.inputValue
+      );
+      nodeListForEach(fields, function(cur) {
+        cur.classList.toggle('red-focus');
+      });
+
+      document.querySelector(DOMStr.inputBtn).classList.toggle('red');
+    },
 
 		getDOMStrings: function() {
 			return DOMStr;
@@ -320,6 +330,8 @@ var controller = (function(budgetCtrl, UICtrl) {
 		});
 
     document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 	};
 
 	var DOM = UICtrl.getDOMStrings();
