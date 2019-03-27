be5bdc2013a258d1ff6a93461e7321e7d07f1293
Rick <3741326+RickJP@users.noreply.github.com>
2019-03-27 10:50:40 +0900

    Resets total income and expenses; displays '%', or '----' if percentage is -1 (i.e. not able to be calculated)

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ var budgetController = (function() {
         totalInc: data.totals.inc,
         totalExp: data.totals.exp,
         percentage: data.percentage
-      }
+      };
     },
 
 
@@ -172,7 +172,12 @@ var UIController = (function() {
       document.querySelector(DOMStr.budgetLabel).textContent = obj.budget;
       document.querySelector(DOMStr.incomeLabel).textContent = obj.totalInc;
       document.querySelector(DOMStr.expensesLabel).textContent = obj.totalExp;
-      document.querySelector(DOMStr.percentageLabel).textContent = obj.percentage;
+      
+      if  (obj.percentage > 0) {
+        document.querySelector(DOMStr.percentageLabel).textContent = obj.percentage + '%';
+      } else {
+          document.querySelector(DOMStr.percentageLabel).textContent = '----'
+      }
     },
 
 		getDOMStrings: function() {
@@ -240,6 +245,12 @@ var controller = (function(budgetCtrl, UICtrl) {
   return {
     init: function() {
       console.log('Application has started.');
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
       setupEventListeners();
     }
   }
