dd34661a39c42f3b98f23cf7308d083dff2ccae7
Rick <3741326+RickJP@users.noreply.github.com>
2019-03-27 09:06:19 +0900

    Added calculateBuget function to calculate totals and percentage

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ var budgetController = (function() {
     this.value = value;
   };
 
+  var calculateTotal = function(type) {
+    var sum = 0;
+    data.allItems[type].forEach((cur) => {
+      sum += cur.value;
+    });
+    data.totals[type] = sum;
+  };
+
   var data = {
     allItems: {
       exp: [],
@@ -21,7 +29,9 @@ var budgetController = (function() {
     totals: {
       exp: 0,
       inc: 0
-    }   
+    },
+    budget: 0,
+    percentage: -1   
   };
 
   return {
@@ -50,6 +60,31 @@ var budgetController = (function() {
       // return element
       return newItem;
     },
+
+    calculateBudget: function() {
+
+      // Calculate total income & expenses
+
+      calculateTotal('inc');
+      calculateTotal('exp');
+
+      // Calculate the budget: income - expenses
+      data.budget = data.totals.inc - data.totals.exp;
+
+      // Calculate the percentage of income that we spent
+      data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+    },
+
+    getBudget: function() {
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalexp: data.totals.enc,
+        percentage: data.percentage
+      }
+    },
+
+
     testing: function() {
       console.log(data);
     }
@@ -145,10 +180,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   var updateBudget = function() {
     // Calculate budget
+    budgetCtrl.calculateBudget();
 
     // Return budget
+    var budget = budgetCtrl.getBudget();
 
     // Display budget on UI
+    console.log(budget);
   }
 
 
